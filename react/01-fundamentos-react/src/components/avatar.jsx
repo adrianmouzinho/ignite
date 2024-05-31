edb7b882c73a543c8511112fdb07fe53809a6130
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-05-31 09:43:27 -0300

    style: change inset shadow to outside shadow on avatar image

--- react/01-fundamentos-react/src/components/avatar.jsx
+++ react/01-fundamentos-react/src/components/avatar.jsx
@@ -1,7 +1,6 @@
 import styles from './avatar.module.css'
 
 export function Avatar({ src, alt, hasBorder = true }) {
-  console.log({ src })
   return (
     <img 
       className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
