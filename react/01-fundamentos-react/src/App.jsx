da9ef3016ad18124f5ad9b0bf5b4e47cf7f0b7eb
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-05-31 10:13:41 -0300

    refactor: add props on Post component

--- react/01-fundamentos-react/src/App.jsx
+++ react/01-fundamentos-react/src/App.jsx
@@ -1,10 +1,40 @@
 import { Header } from './components/header'
 import { Sidebar } from './components/sidebar'
-import { Post } from './components/post';
+import { Post } from './components/post'
 import styles from './app.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-25 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-31 20:00:00'),
+  },
+]
 
 export function App() {
   return (
@@ -13,7 +43,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- react/01-fundamentos-react/src/components/post.jsx
+++ react/01-fundamentos-react/src/components/post.jsx
@@ -1,26 +1,42 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { Avatar } from './avatar'
 import { Comment } from './comment'
+
 import styles from './post.module.css'
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <div className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/adrianmouzinho.png" alt="Adrian Mouzinho" />
+          <Avatar src={author.avatarUrl} alt={author.name} />
           <div>
-            <strong>Adrian Mouzinho</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="29 de Maio às 10:13h" dateTime="2024-05-29 10:13:00">Publicado há 2h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>Publicado {publishedDateRelativeToNow}</time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉<a href="">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        {content.map((block, i) => {
+          if (block.type === 'paragraph') {
+            return <p key={i}>{block.content}</p>
+          } else if (block.type === 'link') {
+            return <p key={i}><a href="">{block.content}</a></p>
+          }
+        })}
       </div>
 
       <form className={styles.form}>
